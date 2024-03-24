0959b0b5cb6a9fa3cf11287c538a3a3c9ea81022
Smark16 <sengendomark16@gmail.com>
2024-03-24 16:05:10 +0300

    changed django version

--- backend/settings.py
+++ backend/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-+@9r1@y)$c0!2yr7yzbd(fhi5q+d%5mjowl$fynjy&5i9=(0%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower = "true"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
